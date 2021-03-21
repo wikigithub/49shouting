f5db8580d1d16e43e6f48313a7db2c1c4c88655f
wikigithub <yunming.lang@gmail.com>
2021-03-21 18:28:53 +0800

    Update taskcallback.js

--- commands/tasks/unicom/taskcallback.js
+++ commands/tasks/unicom/taskcallback.js
@@ -37,7 +37,31 @@ function a(str) {
 var taskcallback = {
     // 查询活动状态
     query: async (axios, options) => {
-        let { params } = options
+        //let { params } = options
+        let params = {
+                arguments1: "AC20200814162815", // acid
+                arguments2: "GGPD", // yhChannel
+                arguments3: "6c54032f662c4d2bb576872ed408232c", // yhTaskId menuId
+                arguments4: new Date().getTime(), // time
+                arguments6: "517050707",
+                arguments7: "517050707",
+                arguments8: "123456",
+                arguments9: "4640b530b3f7481bb5821c6871854ce5",
+                orderId: crypto
+                    .createHash("md5")
+                    .update(new Date().getTime() + "")
+                    .digest("hex"),
+                netWay: "Wifi",
+                remark: "章节视频得积分",
+                version: `android@8.0102`,
+                //orderId: "0923fca6d5ffb8ec017fc6b3cbc5c9c0",
+            };
+            params["sign"] = sign([
+                params.arguments1,
+                params.arguments2,
+                params.arguments3,
+                params.arguments4,
+            ]);
         const useragent = `okhttp/4.4.0`
 
         let { data, config } = await axios.request({
